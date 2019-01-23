34ca883546bd3bb1e3fe2d37f1767c17ef9bc5e7
Melissa Real <melissa.real@hotmail.com>
2019-01-23 15:40:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-function upperCaseCustomerName(){
-
+function upperCaseCustomerName(n){
+  n = customerName;
   return customerName.toUpperCase();
 }
 
